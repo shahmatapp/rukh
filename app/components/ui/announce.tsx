7f9acaf03b10a14ef807cc35f54adad3cf933391
aboosoyeed <soyeed2000@gmail.com>
2025-01-22 21:38:57 +1100

    push all

--- app/components/ui/announce.tsx
+++
@@ -1,9 +0,0 @@
-import SportsIcon from '@mui/icons-material/Sports';
-import {ReactNode} from "react";
-
-interface Props{
-    children: ReactNode;
-}
-export default function Announce({children}:Props){
-    return <div><span className={"text-primary mr-2"}><SportsIcon fontSize={"large"}/></span><span>{children}</span></div>;
-}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"; // This is a client component 👈🏽
-import Home from './home'
-export default function Index() {
-    return (
-        <Home/>
-    )
-}
